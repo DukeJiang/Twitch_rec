cedb727d2b0035d7ffe0d8c1cbfbd8a256d428eb
dukejiang <jyx19980308@gmail.com>
2021-11-08 17:40:51 -0600

    "added recommendationn functionality"

---
+++ src/main/backend/com/dukejiang/twitchRec/recommendation/RecommendationException.java
@@ -0,0 +1,7 @@
+package com.dukejiang.twitchRec.recommendation;
+
+public class RecommendationException extends RuntimeException {
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}

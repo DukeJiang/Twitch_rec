495886a87d8f6f1f940556c44ebaad475347f7e1
dukejiang <jyx19980308@gmail.com>
2021-11-07 23:43:05 -0600

    "modified search servlet and facorite servlet"

--- src/main/backend/com/dukejiang/twitchRec/servlet/FavoriteServlet.java
+++ src/main/backend/com/dukejiang/twitchRec/servlet/FavoriteServlet.java
@@ -1,5 +1,6 @@
 package com.dukejiang.twitchRec.servlet;
 
+import com.dukejiang.twitchRec.entity.Item;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.dukejiang.twitchRec.db.MySQLConnection;
 import com.dukejiang.twitchRec.db.MySQLException;
@@ -12,79 +13,68 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.*;
 
 @WebServlet(name = "FavoriteServlet", urlPatterns = {"/favorite"})
 public class FavoriteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Check if the session is still valid, which means the user has been logged in successfully.
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return;
-        }
-        String userId = (String) session.getAttribute("user_id");
-        // Get favorite item information from request body
-        FavoriteRequestBody body = ServletUtil.readRequestBody(FavoriteRequestBody.class, request);
-        if (body == null) {
+        //Get user ID from request URL, this is a temporary solution since we don't support session now
+        String userId = request.getParameter("user_id");
+
+        //get favorite item information from request body
+        ObjectMapper mapper = new ObjectMapper();
+        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        if(body == null){
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
 
         MySQLConnection connection = null;
-        try {
-            // Save the favorite item to the database
+        try{
+            //Save the favorite item to the database
             connection = new MySQLConnection();
             connection.setFavoriteItem(userId, body.getFavoriteItem());
-        } catch (MySQLException e) {
+        }catch(MySQLException e){
             throw new ServletException(e);
-        } finally {
-            if (connection != null) {
+        }finally {
+            if(connection != null){
                 connection.close();
             }
         }
     }
 
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Check if the session is still valid, which means the user has been logged in successfully.
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return;
-        }
-        String userId = (String) session.getAttribute("user_id");
-        FavoriteRequestBody body = ServletUtil.readRequestBody(FavoriteRequestBody.class, request);
-        if (body == null) {
+        String userId = request.getParameter("user_id");
+        ObjectMapper mapper = new ObjectMapper();
+        FavoriteRequestBody body = mapper.readValue(request.getReader(), FavoriteRequestBody.class);
+        if(body == null){
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
-
         MySQLConnection connection = null;
-        try {
-            // Remove the favorite item to the database
+        try{
+            //Remove the favorite item to the database
             connection = new MySQLConnection();
             connection.unsetFavoriteItem(userId, body.getFavoriteItem().getId());
-        } catch (MySQLException e) {
+        }catch(MySQLException e){
             throw new ServletException(e);
-        } finally {
-            if (connection != null) {
+        } finally{
+            if(connection != null){
                 connection.close();
             }
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Check if the session is still valid, which means the user has been logged in successfully.
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return;
-        }
-        String userId = (String) session.getAttribute("user_id");
+        String userId = request.getParameter("user_id");
+        Map<String, List<Item>> itemMap;
         MySQLConnection connection = null;
         try {
             // Read the favorite items from the database
             connection = new MySQLConnection();
-            ServletUtil.writeItemMap(response, connection.getFavoriteItems(userId));
+            itemMap = connection.getFavoriteItems(userId);
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().print(new ObjectMapper().writeValueAsString(itemMap));
         } catch (MySQLException e) {
             throw new ServletException(e);
         } finally {
